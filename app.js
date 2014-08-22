663816bc375a226d51e67c80453296b7e29e2455
Worawut Boontan <worawut@itopplus.com>
2014-08-23 01:43:40 +0700

    Forget Some File

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(favicon());
+app.use(expressLayouts);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
